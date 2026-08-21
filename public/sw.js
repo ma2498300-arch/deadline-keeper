3adcfae05626cafa0ea746dedf921b339bc0960e
Mohamed <ma2498300@gmail.com>
2026-08-21 10:31:31 +0300

    describe what you changed here

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,30 @@
-const CACHE = 'deadline-keeper-v1'
+const CACHE = 'deadline-keeper-v2'
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/'])))
-  self.skipWaiting()
 })
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim())
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))),
+    self.clients.claim(),
+  ]))
+})
+
+self.addEventListener('message', event => {
+  if (event.data?.type === 'SKIP_WAITING') self.skipWaiting()
 })
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).then(response => {
+      const copy = response.clone()
+      caches.open(CACHE).then(cache => cache.put(event.request, copy))
+      return response
+    }).catch(() => caches.match(event.request).then(cached => cached || caches.match('/'))))
+    return
+  }
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
     const copy = response.clone()
     caches.open(CACHE).then(cache => cache.put(event.request, copy))
